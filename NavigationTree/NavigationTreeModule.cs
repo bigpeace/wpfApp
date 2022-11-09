bb010524244b88bf183c074683bbfd457a7153f6
Yamato-Suzuki <44799293+bigpeace@users.noreply.github.com>
2022-11-10 08:21:33 +0900

    first commit

---
+++ NavigationTree/NavigationTreeModule.cs
@@ -0,0 +1,22 @@
+﻿using NavigationTree.Views;
+using Prism.Ioc;
+using Prism.Modularity;
+using Prism.Regions;
+
+namespace NavigationTree
+{
+    public class NavigationTreeModule : IModule
+    {
+        public void OnInitialized(IContainerProvider containerProvider)
+        {
+            var regionMan = containerProvider.Resolve<IRegionManager>();
+            regionMan.RegisterViewWithRegion("NaviTree", typeof(Views.NavigationTree));
+
+        }
+
+        public void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+
+        }
+    }
+}
